b9c52cf51c9aa25f1e5e296bc67a2316bd5608cc
droplet92 <lhs1438@gmail.com>
2023-05-04 17:03:55 +0900

    경로 매크로 사용 (나중에는 프로젝트 파일 ignore에 추가하기)

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -23,7 +23,7 @@
  ****************************************************************************/
 
 #include "AppDelegate.h"
-#include "TitleScene.h"
+#include "Scene/TitleScene.h"
 
 // #define USE_AUDIO_ENGINE 1
 
